7fdf3e2148b9e4018a1a3d7122b8b09ce4852976
Eric Gallager <egall@gwmail.gwu.edu>
2023-07-30 15:58:31 -0400

    Done with current copyediting pass

---
+++ translations/msgmerge_pofiles.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# First, run wmlxgettext from your wesnoth tools directory like this:
+# ./wmlxgettext --domain=wesnoth-Dawn_of_Thunder --directory=/Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.16/data/add-ons/dawn_of_thunder --recursive --warnall -o /Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.16/data/add-ons/dawn_of_thunder/translations/Dawn_of_Thunder.pot
+# (with edits to correct paths as necessary)
+
+for mylang in ja; do
+	msgmerge --previous --update --lang=${mylang} wesnoth-Dawn_of_Thunder/${mylang}.po Dawn_of_Thunder.pot;
+	msgfmt -o wesnoth-Dawn_of_Thunder/${mylang}.mo wesnoth-Dawn_of_Thunder/${mylang}.po;
+done
+if test -e ~/Downloads/Dawn_of_Thunder_old.pot; then
+	diff -u ~/Downloads/Dawn_of_Thunder_old.pot Dawn_of_Thunder.pot > Dawn_of_Thunder.pot.diff
+fi
